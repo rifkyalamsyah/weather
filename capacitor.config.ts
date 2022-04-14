0d6de167808f9d8a823367130c103222cb727cca
Rifky Alamsyah <rifkyalamsyah30@gmail.com>
2022-04-14 10:40:39 +0700

    Weather Now

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.weather',
+  appName: 'weather',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  url = 'https://api.openweathermap.org/data/2.5/';
+  key = '324906282ee9dbb3ffd24dcb55399f34';
+  city = 'Sleman';
+
+  constructor(private http: HttpClient) { }
+
+  getData(){
+    return this.http.get(`${this.url}weather?q=${this.city}&appid=${this.key}&units=metric`);
+  }
 }
